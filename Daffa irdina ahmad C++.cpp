f8138ffd44f2ad9d8973421ef387e84bca4dbc81
daffaajhh <141287109+daffaajhh@users.noreply.github.com>
2023-10-04 13:35:22 +0700

    Add files via upload

---
+++ Daffa irdina ahmad C++.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    double luas;
+    int alas;
+    int tinggi;
+
+    cin >> tinggi;
+    cin >> alas;
+    luas = alas * tinggi * 0.5;
+    cout << luas << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
